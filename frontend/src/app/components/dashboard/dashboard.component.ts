df8d4ad833e1f77d419204aac8f964414d7cc8f5
Gerardo Llamas <tu-correo@ejemplo.com>
2026-07-31 01:53:01 -0600

    pantalla despues de login

---
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  usuario$;
+
+  constructor(private authService: AuthService, private router: Router) {
+    this.usuario$ = this.authService.usuarioActual$;
+  }
+
+  cerrarSesion(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
