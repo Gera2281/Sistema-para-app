96c498aead239d30fbdf0a77db5c7be9a164310f
Gerardo Llamas <tu-correo@ejemplo.com>
2026-08-04 20:56:46 -0600

    rutas front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsersComponent } from './components/users/users.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UsersComponent },
+  { path: 'reset-password/:token', component: ResetPasswordComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
